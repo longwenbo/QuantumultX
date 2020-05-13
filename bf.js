b298025f9e2055ef6db69c22978a268a54408fe4
longwenbo <40166561+longwenbo@users.noreply.github.com>
2020-05-13 22:43:48 +0800

    Update bf.js

--- bf.js
+++ bf.js
@@ -1,7 +1,21 @@
+/*
+https?:\/\/apa\.bfzyapp\.com url script-response-body bf.js
+https?:\/\/apa\.bfzyapp\.com url script-request-body bf.js
+MITM = apa.bfzyapp.com
+*/
 let url = JSON.parse($request.url);
 let requestObj = JSON.parse($request.body);
 let responseObj = JSON.parse($response.body);
-if(url.indexOf("femalePrivacyAuth")){
-  
+if(url.indexOf("/femalePrivacyAuth") != -1){
+  requestObj.type = "3";
+  responeseObj.showPrivateAuthStatus = 3;
+}
+if(url.indexOf("/viewUser") != -1){
+  responseObj.type = 1;
+}
+if(url.indexOf("/sendVoiceRequest") != -1){
+  responseObj.price=0;
+  responseObj.coin=9999;
+  responseObj.allowStatus=1;
 }
 $done({body:JSON.stringify(obj)});
