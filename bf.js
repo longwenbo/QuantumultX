fc8040d7652b4e45b4d2e7b7c0cd05c4bacb40a8
longwenbo <40166561+longwenbo@users.noreply.github.com>
2020-05-14 17:40:47 +0800

    Update bf.js

--- bf.js
+++ bf.js
@@ -7,12 +7,12 @@ var url = $request.url;
 let requestObj = JSON.parse($request.body);
 var responseObj = JSON.parse($response.body);
 if(url.indexOf("/femalePrivacyAuth") != -1){
-  requestObj.dta.type = "3";
+  requestObj.type = "3";
   responseObj.showPrivateAuthStatus = 3;
 //   $done({body:JSON.stringify(requestObj)});
 }
 if(url.indexOf("/viewUser") != -1){
-  responseObj.data.type = 2;
+  responseObj.type = 2;
 }
 if(url.indexOf("/sendVoiceRequest") != -1){
   responseObj.data.price=0;
