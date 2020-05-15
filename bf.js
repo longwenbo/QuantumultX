89e59a63533f7f697c20d8807ebba0745fe856e8
lwb <735620461@qq.com>
2020-05-15 10:02:25 +0800

    百分之一

--- bf.js
+++ bf.js
@@ -1,3 +1,27 @@
-let obj = JSON.parse($request.body);
-obj.type = "2";
-$done({body:JSON.stringify(obj)});
+/*
+https?:\/\/apa\.bfzyapp\.com url script-response-body bo/bf.js
+https?:\/\/apa\.bfzyapp\.com url script-request-body bf.js
+MITM = apa.bfzyapp.com
+*/
+var url = $request.url;
+// requestObj = JSON.parse($request.body);
+var obj = JSON.parse($response.body);
+
+if(url.indexOf("/femalePrivacyAuth") != -1){
+  obj.data.showPrivateAuthStatus = 3;
+}
+if(url.indexOf("/viewUser") != -1){
+  for(let i=0;i<obj.data.photos.length;i++){
+    obj.data.photos[i].type=1;
+}
+}
+
+if(url.indexOf("sendVoiceRequest") != -1){
+  obj.data.price=0;
+  obj.data.coin=9999;
+  obj.data.allowStatus=1;
+}
+
+$done({
+	body: JSON.stringify(obj)
+});
